3441ccf5dfb74a6c39fad3013e22fcfb211f7658
Suha523 <suhashehadeh123@gmail.com>
2022-11-13 18:42:13 +0200

    add post and show all addvertises features

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,40 +1,47 @@
-import './App.css';
-import axios from 'axios';
-import { useEffect, useState } from 'react';
-import {BrowserRouter, Routes, Route} from 'react-router-dom'
-import MyNav from './components/Navbar/Navbar';
-import AddAddvForm from './components/AddAddvForm/AddAddvForm';
-import Search from './components/Search/Search';
+import "./App.css";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import MyNav from "./components/Navbar/Navbar";
+import AddAddvForm from "./components/AddAddvForm/AddAddvForm";
+import Search from "./components/Search/Search";
 
 function App() {
-  const[addvertises, setAddvertises] = useState([])
+  const [addvertises, setAddvertises] = useState([]);
 
-  useEffect(() => { 
-    axios.get('http://localhost:4000/addvertises').then((response) => {
-        setAddvertises(response.data)
-    })
-  }, [])
+  useEffect(() => {
+    getAddvertises();
+  }, []);
 
-  const postAddvertise = (addvertiseData) => {
-    axios.post('http://localhost:4000/addvertises', addvertiseData).then((response) => {
-        let addvertise = response.data
-        setAddvertises(addvertises.push(addvertise))
-    })
+  const getAddvertises = () => {
+    axios.get("http://localhost:4000/addvertises").then((response) => {
+      setAddvertises(response.data);
+    });
   };
-  
+  const postAddvertise = (addvertiseData) => {
+    axios
+      .post("http://localhost:4000/addvertises", addvertiseData)
+      .then((response) => {
+        let addvertise = response.data;
+        setAddvertises((prevState) => [...prevState, addvertise]);
+        getAddvertises();
+      });
+  };
+
   return (
-    <div className="App">
-          <BrowserRouter>
-            <MyNav />
-            <div className='d-flex justify-content-between m-5'>
-                <Routes>
-                  <Route path='/' element={<Search addvertises={addvertises} />} />
-                  <Route path='/addAddvForm' element={<AddAddvForm postAddvertise={postAddvertise} />} />
-              </Routes>
-            </div>
-          </BrowserRouter>
-       
-     
+    <div className='App'>
+      <BrowserRouter>
+        <MyNav />
+        <div className='d-flex justify-content-between m-5'>
+          <Routes>
+            <Route path='/' element={<Search addvertises={addvertises} />} />
+            <Route
+              path='/addAddvForm'
+              element={<AddAddvForm postAddvertise={postAddvertise} />}
+            />
+          </Routes>
+        </div>
+      </BrowserRouter>
     </div>
   );
 }
